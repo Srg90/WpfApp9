d3f711ff97b4825c556ce81a364ca7d5c84f850a
Srg90 <usikov90@mail.ru>
2021-12-23 23:14:52 +0300

    Добавьте файлы проекта.

---
+++ CommandsWPF7.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp9
+{
+    class CommandsWPF7
+    {
+        public static RoutedUICommand Close { get; set; }
+        public static RoutedCommand Bold { get; set; }
+        public static RoutedCommand Italic { get; set; }
+        public static RoutedCommand Underline { get; set; }
+        public static RoutedCommand Color { get; set; }
+        static CommandsWPF7()
+        {
+            InputGestureCollection inputs = new InputGestureCollection();
+            inputs.Add(new KeyGesture(Key.T, ModifierKeys.Control, "Ctrl+T"));
+            Close = new RoutedUICommand("Выход", "Выход", typeof(CommandsWPF7), inputs);
+            Bold = new RoutedCommand();
+            Italic = new RoutedCommand();
+            Underline = new RoutedCommand();
+            Color = new RoutedCommand();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+
+namespace WpfApp9
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<string> themes = new List<string>() { "Standart Theme", "Dark Theme", "Light Theme" };
+            ComboTheme.ItemsSource = themes;
+            //ComboFN.ItemsSource = new string[5] { "Arial", "Times New Roman", "Verdana", "Comic Sans MS", "Gabriola" };
+            //ComboFS.ItemsSource = new int[14] { 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 36, 48, 72 };
+            ComboTheme.SelectionChanged += Select3;
+            ComboTheme.SelectedIndex = 0;
+        }
+        private void Select1(object sender, SelectionChangedEventArgs e)
+        {
+            string fontName = ((sender as ComboBox).SelectedItem).ToString();
+            if (textBox != null)
+            {
+                textBox.FontFamily = new FontFamily(fontName);
+            }
+        }
+
+        private void Select2(object sender, SelectionChangedEventArgs e)
+        {
+            string fontSize = ((sender as ComboBox).SelectedItem).ToString();
+            double FS = Double.Parse(fontSize);
+            if (textBox != null)
+            {
+                textBox.FontSize = FS;
+            }
+        }
+        private void Select3(object sender, SelectionChangedEventArgs e)
+        {
+            //string themeName = ((sender as ComboBox).SelectedItem).ToString();
+
+            int styleIndex = ComboTheme.SelectedIndex;
+            Uri uri = new Uri("StandartTheme.xaml", UriKind.Relative);
+            if (styleIndex == 1) { uri = new Uri("DarkTheme.xaml", UriKind.Relative); }
+            else if(styleIndex == 2) { uri = new Uri("LightTheme.xaml", UriKind.Relative); }
+
+            ResourceDictionary resource = Application.LoadComponent(uri) as ResourceDictionary;
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resource);
+
+        }
+        private void ClickBold(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (bold.IsChecked == true)
+            {
+                textBox.FontWeight = FontWeights.Bold;
+            }
+            else
+            {
+                textBox.FontWeight = FontWeights.Regular;
+            }
+        }
+        private void ClickItalic(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (italic.IsChecked == true)
+            {
+                textBox.FontStyle = FontStyles.Italic;
+            }
+            else
+            {
+                textBox.FontStyle = FontStyles.Normal;
+            }
+        }
+        private void ClickUnderline(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (underline.IsChecked == true)
+            {
+                textBox.TextDecorations = TextDecorations.Underline;
+            }
+            else
+            {
+                textBox.TextDecorations = null;
+            }
+        }
+        private void Color(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (textBox != null & colorBlack.IsChecked == true)
+            {
+                textBox.Foreground = Brushes.Black;
+            }
+            else if (textBox != null & colorBlack.IsChecked != true)
+            {
+                textBox.Foreground = Brushes.Red;
+            }
+        }
+        private void Open(object sender, ExecutedRoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Text Format (*.txt)|*.txt|All files (*.*)|*.*";
+            if (dlg.ShowDialog() == true)
+            {
+                textBox.Text = File.ReadAllText(dlg.FileName);
+            }
+        }
+        private void Save(object sender, ExecutedRoutedEventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.Filter = "Text Format (*.txt)|*.txt|All files (*.*)|*.*";
+            if (dlg.ShowDialog() == true)
+            {
+                File.WriteAllText(dlg.FileName, textBox.Text);
+            }
+        }
+        private void Print(object sender, ExecutedRoutedEventArgs e)
+        {
+            PrintDialog printDialog = new PrintDialog();
+            if (printDialog.ShowDialog() == true)
+            {
+                FlowDocument flowDocument = new FlowDocument();
+                flowDocument.PagePadding = new Thickness(50);
+                flowDocument.Blocks.Add(new Paragraph(new Run(textBox.Text)));
+
+                printDialog.PrintDocument((((IDocumentPaginatorSource)flowDocument).DocumentPaginator), "Print");
+            }
+        }
+        private void Close(object sender, ExecutedRoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
